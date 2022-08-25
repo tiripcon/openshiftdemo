fb750b8a5e8b316a632c3321950e879b7ed6bce8
RIPCONCIV\fnieto <fnieto@ripconciv.com>
2022-08-25 14:54:08 -0500

    Commit APP DEMO OS

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css';
+import Usuarios from './componentes/Usuarios.jsx'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Usuarios />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componentes/Usuarios.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Usuarios = () => {
+  return (
+    <div>APLICACION DEMO DE REACT PARA OPENSHIFT</div>
+  )
+}
+
+Usuarios.propTypes = {}
+
+export default Usuarios
